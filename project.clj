7f3da9fc231edd1c6324a907ad3fa6a29e2e5f50
mocchi <boom.boom.planet@gmail.com>
2015-08-22 20:34:24 +0900

    remove mainclass

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject slack-ghost "0.1.0-SNAPSHOT"
+(defproject slack-ghost "0.2.0-SNAPSHOT"
   :description "slack bot"
   :url "https://github.com/mocchit/ghost"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main ghost.core
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [stylefruits/gniazdo "0.4.0"]
                  [org.julienxx/clj-slack "0.4.3"]])
--- src/slack_ghost/core.clj
+++ src/slack_ghost/core.clj
@@ -1,12 +1,2 @@
 (ns slack-ghost.core
-  (:gen-class main true)
   (:require [slack-ghost.bot-core :as bot]))
- 
-(defn print-message [this data]
- (println (:text data)))
-
-(defn -main [& args]
-  (let [token (first args)
-        event {:message [#'print-message]}
-       bot (bot/constructor-bot token event)]
-    (bot/connect bot)))
