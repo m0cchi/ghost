a408ea9a1da4f56d8bb965b1d301e25653a7f31c
mocchit <boom.boom.planet@gmail.com>
2015-08-23 02:22:33 +0900

    update project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slack-ghost "0.3.0-SNAPSHOT"
+(defproject slack-ghost "0.3.1-SNAPSHOT"
   :description "slack bot"
   :url "https://github.com/mocchit/ghost"
   :license {:name "Eclipse Public License"
