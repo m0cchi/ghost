76fff4d4d4e21c5e5ee7e238f815528b4bfa2df1
mocchi <boom.boom.planet@gmail.com>
2016-02-28 22:27:24 +0900

    fix

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mocchit/ghost"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [stylefruits/gniazdo "0.4.0"]
                  [org.julienxx/clj-slack "0.5.2.1"]])
 
